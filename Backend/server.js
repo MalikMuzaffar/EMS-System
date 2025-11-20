2c73ccdcfaafb746c180a14c3ff1a5e0c859bd66
MalikMuzaffar <muzaffaribrar182@gmail.com>
2025-11-20 12:31:39 +0500

    change port in server.js

--- Backend/server.js
+++ Backend/server.js
@@ -46,6 +46,11 @@ markAbsentsJob();
 // Multer limits
 const upload = multer({ limits: { fileSize: 100 * 1024 * 1024 } }); // 100MB
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
 
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`✅ Server running on port ${PORT}`);
+});
